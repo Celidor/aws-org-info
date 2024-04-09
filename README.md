647f02b85478ab01ce36c090dbaf20f43c0c8dcf
Paul Schwarzenberger <paul@celidor.net>
2024-04-09 09:11:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# aws-org-access
+* enumerates AWS accounts in an org and reports on those with the default `OrganizationAccountAccessRole`
+
+## setup
+```
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## usage
+* assume a role into the AWS org management account
+* open CloudShell
+```
+python org_access.py
+```
---
+++ org_access.py
@@ -0,0 +1,20 @@
+from botocove import cove
+
+
+@cove()
+def get_aws_account_id(session):
+    sts = session.client("sts")
+    return sts.get_caller_identity()["Account"]
+
+def main():
+    # No session passed as the decorator injects it
+    all_results = get_aws_account_id()
+
+    # Returns a Dict with keys Results, Exceptions and FailedAssumeRole
+    print("Accounts with org access role: ", len(all_results["Results"]))
+    # print("Total exceptions: ", len(all_results["Exceptions"]))
+    print("Accounts without org access roles: ", len(all_results["FailedAssumeRole"]))
+    print("Total accounts:" , len(all_results["Results"]) + len(all_results["FailedAssumeRole"]) + 1)
+
+if __name__ == "__main__":
+    main()
